3b9b57e5322b197b99c9d05a014cd1d80e8e1f74
YupengTUM <y-zhao@t-online.de>
2019-02-08 15:25:35 +0100

    Table app

---
+++ personadialog.cpp
@@ -0,0 +1,14 @@
+#include "personadialog.h"
+#include "ui_personadialog.h"
+
+PersonaDialog::PersonaDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::PersonaDialog)
+{
+    ui->setupUi(this);
+}
+
+PersonaDialog::~PersonaDialog()
+{
+    delete ui;
+}
